5eca7ea80f16ccf6e5101821fc773a19b4b088e0
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-09-26 14:54:16 -0300

    Update Order.ts

--- src/app/models/Order.ts
+++ src/app/models/Order.ts
@@ -1,31 +1,39 @@
-import {model, Schema } from 'mongoose';
+//Importa os métodos 'model' e 'Schema' do pacote 'mongoose'
+import { model, Schema } from 'mongoose';
 
+//Cria um modelo chamado 'Order' utilizando o método 'model'
 export const Order = model('Order', new Schema({
-	table: {
-		type: String,
-		required: true,
-	},
-	status: {
-		type: String,
-		enum: ['WAITING', 'IN_PRODUCTION', 'DONE'],
-		default: 'WAITING',
-	},
-	creatdAt: {
-		type: Date,
-		default: Date.now,
-	},
-	products: {
-		required: true,
-		type:[{
-			product: {
-				type: Schema.Types.ObjectId,
-				required: true,
-				ref: 'Product',
-			},
-			quantity: {
-				type: Number,
-				default: 1,
-			},
-		}],
-	},
+    // Definindo o campo 'table' do tipo String e obrigatório
+    table: {
+        type: String,
+        required: true,
+    },
+    //Define o campo 'status' do tipo String, com valores enumerados e um valor padrão
+    status: {
+        type: String,
+        enum: ['WAITING', 'IN_PRODUCTION', 'DONE'], 
+        default: 'WAITING', 
+    },
+    //Define o campo 'createdAt' do tipo Date, com um valor padrão de data atual
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    //Define o campo 'products' que é um array de objetos com 'product' e 'quantity'
+    products: {
+        required: true,
+        type: [{
+            //Cada objeto tem um campo 'product' do tipo ObjectId que faz referencia o modelo 'Product'
+            product: {
+                type: Schema.Types.ObjectId,
+                required: true,
+                ref: 'Product', // Referencia ao modelo 'Product'
+            },
+            //e um campo 'quantity' do tipo Number, com um valor padrão de 1
+            quantity: {
+                type: Number,
+                default: 1,
+            },
+        }],
+    },
 }));
