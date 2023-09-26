18c05b08aca9261192d2411c7d26f22530c2c8b0
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-09-26 14:50:15 -0300

    Update Category.ts

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,12 +1,16 @@
-import {model, Schema } from 'mongoose';
+//Importa os métodos 'model' e 'Schema' do pacote 'mongoose'
+import { model, Schema } from 'mongoose';
 
+//Cria um modelo chamado 'Category' utilizando o método 'model'
 export const Category = model('Category', new Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	icon: {
-		type: String,
-		required: true,
-	}
+    // Definindo o campo 'name' do tipo String e obrigatório
+    name: {
+        type: String,
+        required: true,
+    },
+    //Define o campo 'icon' do tipo String e obrigatório
+    icon: {
+        type: String,
+        required: true,
+    }
 }));
